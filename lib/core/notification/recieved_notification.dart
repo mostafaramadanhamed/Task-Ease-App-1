d6b0018b623707f34d559ee55bdea6c34fa41524
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-07-04 11:17:40 +0300

    add local time zone

--- lib/core/notification/recieved_notification.dart
+++ lib/core/notification/recieved_notification.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_timezone/flutter_timezone.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 class NotificationHelper {
@@ -31,8 +34,14 @@ class NotificationHelper {
 // Assuming you have a desired DateTime object (e.g., scheduledTime)
     // Optional: Use user's location
     tz.initializeTimeZones();
-    var scheduledDateTime = tz.TZDateTime.from(scheduledTime, tz.getLocation(scheduledTime.timeZoneName));
+    final String currentTimeZone = await FlutterTimezone.getLocalTimezone();
+    tz.setLocalLocation(tz.getLocation( currentTimeZone));
+    log(tz.local.toString());
+    log(scheduledTime.timeZoneName);
+    log(tz.local.currentTimeZone.toString());
+    log(tz.TZDateTime.now(tz.local).hour.toString());
+  //  var scheduledDateTime = tz.TZDateTime.from(scheduledTime, tz.getLocation(scheduledTime.timeZoneName));
     // Use local time zone if no user location
-    await _plugin.zonedSchedule(id, title, body,scheduledDateTime ,notificationDetails, uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime, androidScheduleMode: AndroidScheduleMode.exact);
+    await _plugin.zonedSchedule(id, title, body,tz.TZDateTime(tz.local, scheduledTime.year,scheduledTime.month,scheduledTime.day,scheduledTime.hour,scheduledTime.minute) ,notificationDetails, uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.wallClockTime, androidScheduleMode: AndroidScheduleMode.alarmClock);
   }
 }
